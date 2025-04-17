a0f3ad1c454ce971ec53e04f927f52f2414fada4
recluse-so <asaunders@spideroak.com>
2025-04-16 17:31:57 -0700

    added support

--- k3s/install.sh
+++ k3s/install.sh
@@ -22,14 +22,14 @@ curl -sfL https://get.k3s.io | sh -s - server \
     --cluster-init
 
 # GET NODES 
-kubectl get nodes
+sudo kubectl get nodes
 
 # get the token and copy paste it into the k3s/config.yaml file
-cat /var/lib/rancher/k3s/server/token
+sudo cat /var/lib/rancher/k3s/server/token
 
 exit 
 
-TOKEN_AGENT=PASTE_TOKEN_HERE
+export TOKEN_AGENT=K10d29b3865afb8743cbe646492eda4cbba5c5261d1f6f21f6f855b0ccbe32f5778::server:sNmxP4YPF+LjLh8ZDOx3pebzgmcj4GcmPfVI1eA3m2k=
 
 # create a k3s agent
 multipass launch --name k3sAgent --cpus 2 --memory 2G --disk 10G
@@ -38,18 +38,23 @@ multipass launch --name k3sAgent --cpus 2 --memory 2G --disk 10G
 multipass list
 
 # set the server ip address
-export SERVER_IP=192.168.64.2
-export TOKEN=toadfss
+export SERVER_IP=https://192.168.64.2:6443
 
 # login to the k3s agent
 multipass shell k3sAgent
 
 # install k3s on the agent
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.30.8+k3s1" \
-    INSTALL_K3S_EXEC="agent--server $SERVER_IP --token $TOKEN" sh - 
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.64.2:6443 --token K10d29b3865afb8743cbe646492eda4cbba5c5261d1f6f21f6f855b0ccbe32f5778::server:sNmxP4YPF+LjLh8ZDOx3pebzgmcj4GcmPfVI1eA3m2k=" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent" K3S_TOKEN="mypassword" sh -s - --server https://k3s.example.com
 
 # install k3s 
 kubectl get nodes
 
 
+sudo journalctl -u k3s-agent -n 100
 
+# check the status of the agent
+sudo systemctl status k3s-agent.service
+
+# check the logs of the agent
+sudo journalctl -xeu k3s-agent.service
